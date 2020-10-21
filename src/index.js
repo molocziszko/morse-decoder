b1ab12db4310c90a6246f091b44abc8b6d5b3f88
molocziszko <Vovvich91@gmail.com>
2020-10-21 13:55:30 +0300

    feat: solution

--- src/index.js
+++ src/index.js
@@ -38,7 +38,27 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    /* constants for regExp to find spaces, 10's, 11's and zeros*/
+    const space = /\*{10}/g;
+    const ten = /10/g;
+    const eleven = /11/g;    
+    const zero = /0/g;
+
+    const decodedMsg = [];              // here will be resulting decoded string
+    const charLen = expr.length / 10;   // every char has 10 binaries
+    const arr = [];                     // intermediary array for splitted binaries    
+    
+    for (let i = 0; i < charLen; i++) {
+      const start = i * 10;
+      const end = (i + 1) * 10;
+      const subStr = expr.substring(start, end);
+      arr.push(subStr);
+    }    
+    /* we find spaces, 10's, 11's and zeros and replace them to respective chars */
+    const resArr = arr.map((char) => char.replace(space, ' ').replace(ten, '.').replace(eleven, '-').replace(zero, ''));    
+    resArr.forEach((char) => decodedMsg.push(MORSE_TABLE[char] || ' ')); // add chars or spaces to decoded string
+    
+    return(decodedMsg.join('')); // join 10's blocks together. Well done!
 }
 
 module.exports = {
